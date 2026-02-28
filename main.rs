1be0f04f8ab94013f89bc9c3c0c362a067526935
Wancher <54241547+troshxa@users.noreply.github.com>
2026-02-28 08:27:52 +0200

    added luc func

--- main.rs
+++ main.rs
@@ -1,10 +1,19 @@
 fn main() {
+    println!("Some fib numbers:");
     println!("{}", fib(0));
     println!("{}", fib(1));
     println!("{}", fib(2));
     println!("{}", fib(3));
     println!("{}", fib(4));
     println!("{}", fib(5));
+
+    println!("Some luc numbers:");
+    println!("{}", luc(0));
+    println!("{}", luc(1));
+    println!("{}", luc(2));
+    println!("{}", luc(3));
+    println!("{}", luc(4));
+    println!("{}", luc(5));
 }
 
 fn fib(n: i32) -> i32 {
@@ -16,9 +25,12 @@ fn fib(n: i32) -> i32 {
     fib(n - 1) + fib(n - 2)
 }
 
-fn fibe(n: i32) -> i32 {
-    if n == 0 || n == 1 {
+fn luc(n: i32) -> i32 {
+    if n == 0{
+        return 2;
+    } else if n== 1{
         return 1;
     }
-    fib(n - 1) + fib(n - 2)
+    luc(n - 1) + luc(n - 2)
 }
+
