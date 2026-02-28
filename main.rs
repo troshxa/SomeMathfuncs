5e7a8ee98ff6533882724c4a3407e540c6b3de26
Wancher <54241547+troshxa@users.noreply.github.com>
2026-02-28 08:22:44 +0200

    corrected fib func..

--- main.rs
+++ main.rs
@@ -8,6 +8,15 @@ fn main() {
 }
 
 fn fib(n: i32) -> i32 {
+    if n == 0{
+        return 0;
+    } else if n== 1{
+        return 1;
+    }
+    fib(n - 1) + fib(n - 2)
+}
+
+fn fibe(n: i32) -> i32 {
     if n == 0 || n == 1 {
         return 1;
     }
